65f587097d75724502b3fbb66ed7cb4a2933646c
Sami Laitinen <sami.p.laitinen@gmail.com>
2024-12-20 20:58:49 +0200

    controllers added

--- index.js
+++ index.js
@@ -3,28 +3,12 @@ const app = express()
 const cors = require('cors')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
-const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
