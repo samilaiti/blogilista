fb94e5a0826a72ae13509c5e5857d90cf981825b
Sami Laitinen <sami.p.laitinen@gmail.com>
2024-12-20 21:22:44 +0200

    middleware added

--- models/blog.js
+++ models/blog.js
@@ -2,13 +2,6 @@ const logger = require('../utils/logger')
 const config = require('../utils/config')
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
-
-const url = config.MONGODB_URI
-
-logger.info('connecting to', url)
-mongoose.connect(url)
-
 const blogSchema = mongoose.Schema({
   title: String,
   author: String,
