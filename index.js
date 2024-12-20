6fd71d0504712ed22ffbf50fdc42a5f749508352
Sami Laitinen <sami.p.laitinen@gmail.com>
2024-12-20 21:08:33 +0200

    app added

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const logger = require('./utils/logger')
+const app = require('./app')
 const config = require('./utils/config')
-const blogsRouter = require('./controllers/blogs')
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
+const logger = require('./utils/logger')
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
