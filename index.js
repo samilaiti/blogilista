e8f851f4069d1d61d9cd80a561798260f012574c
Sami Laitinen <sami.p.laitinen@gmail.com>
2024-12-20 20:24:50 +0200

    utils defined

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-require('dotenv').config()
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+// require('dotenv').config()
 
 const blogSchema = mongoose.Schema({
   title: String,
@@ -38,6 +40,6 @@ app.post('/api/blogs', (request, response) => {
 })
 
 const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
